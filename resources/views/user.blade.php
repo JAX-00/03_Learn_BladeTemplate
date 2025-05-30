77b7b53d3ce7893b580d4af6dbda4599ea94942c
jubi20000905 <66117384+jubi09@users.noreply.github.com>
2025-05-30 10:06:01 +0700

    continue to learn logical in laravel each and chechked

---
+++ resources/views/user.blade.php
@@ -0,0 +1,17 @@
+@production
+    <style>
+        .red {
+            color: red
+        }
+    </style>
+@endonce
+
+<h1>
+    {{ $user['name'] }}
+</h1>
+
+<ul>
+    @foreach ($user['hobbies'] as $hobby)
+        <li class="red">{{ $hobby }}</li>
+    @endforeach
+</ul>
